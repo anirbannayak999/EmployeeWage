d123e00d9d4d623a9b674b4600b65bf211c42455
“anirban <anirbannayak999@gmail.com>
2022-12-23 00:59:16 +0530

    employeedailywagecalculateaaa

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,14 +1,19 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    public static void main(String[] args)
+    {int WAGE_PER_HR = 20;
+        int FullDayHours = 0;
         int employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
         System.out.println(employeeCheck);
         if (employeeCheck == 1) {
             System.out.println("Employee is Present");
+            FullDayHours = 8;
         } else {
             System.out.println("Employee is Absent");
         }
+        int wage = FullDayHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
+
+        }
     }
-}
